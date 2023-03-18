e8e87621dd46d91a438ed157c826d4538341a435
pybuk <pybuuuk@gmail.com>
2023-03-18 23:41:52 +1000

    Make a border lines

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import pygame
 from room_gen import rand_map
-# from map_hitbox import map_lines
+
+WIDTH = 1280
+HEIGHT = 768
+
 
 pygame.init()
-screen = pygame.display.set_mode((1280, 768)) # flags = pygame.NOFRAME
+screen = pygame.display.set_mode((WIDTH, HEIGHT)) # flags = pygame.NOFRAME
 pygame.display.set_caption("FEFUgame")
 icon = pygame.image.load("img/icons/icon100.png")
 pygame.display.set_icon(icon)
 
 bg = pygame.image.load("img/rooms/urdl.jpeg")
 player = pygame.image.load("img/players/dimochka.png")
-# vanechka = pygame.image.load("img/players/vanechka.png")
 
 
 player1_x = 540
@@ -18,8 +20,6 @@ player1_y = 300
 
 player_hitbox = player.get_rect(topleft = (player1_x, player1_y))
 
-# player2_x = 340
-# player2_y = 100
 
 player_speed = 0.5
 player_speed_diag = 0.4
@@ -31,14 +31,9 @@ while running:
     screen.fill("Grey")
     screen.blit(bg, (0, 0))
 
-    lines = [pygame.draw.rect(bg, "RED", (230, 160, 346, 32)),
-        pygame.draw.rect(bg, "RED", (707, 160, 346, 32)),
-        pygame.draw.rect(bg, "RED", (230, 670, 346, 32)),
-        pygame.draw.rect(bg, "RED", (707, 670, 346, 32)),
-        pygame.draw.rect(bg, "RED", (230, 160, 32, 220)),
-        pygame.draw.rect(bg, "RED", (1028, 160, 32, 220)),
-        pygame.draw.rect(bg, "RED", (230, 510, 32, 190)),
-        pygame.draw.rect(bg, "RED", (1028, 510, 32, 190))]
+    l1 = pygame.draw.line(bg, "Red", [256, 192], [574, 192])
+
+
 
     screen.blit(player, (player1_x, player1_y))
 
@@ -60,17 +55,21 @@ while running:
         player1_y -= player_speed_diag
 
     elif keys[pygame.K_a]:
-        player1_x -= player_speed 
+        if player1_x > 246:
+            player1_x -= player_speed
     elif keys[pygame.K_d]:
-        player1_x += player_speed
+        if player1_x < 970:
+            player1_x += player_speed
     elif keys[pygame.K_s]:
-        player1_y += player_speed
+        if player1_y < 614:
+            player1_y += player_speed
     elif keys[pygame.K_w]:
-        player1_y -= player_speed
+        if player1_y > 192:
+            player1_y -= player_speed
 
 
 
-
+    print(player1_x, player1_y)
     pygame.display.update()
 
     for event in pygame.event.get():
--- map_hitbox.py
+++
@@ -1,8 +0,0 @@
-# import pygame
-
-# def map_lines(player, lines):
-#     for line in lines:
-#         if player.contains(line):
-#             return False
-#         else:
-#             return True
