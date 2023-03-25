aad70f091b992b714310845236ab2b8783bbdde6
pybuk <pybuuuk@gmail.com>
2023-03-25 10:21:41 +1000

    smth

--- class_files/classes.py
+++ class_files/classes.py
@@ -15,99 +15,44 @@ class Player(Character):
     def __init__(self, texture, hitbox, hp, damage, speed, diagonal_speed, shield, x, y):
         super().__init__(texture, hitbox, hp, damage, speed, diagonal_speed, x, y)
         self.shield = shield
+
     def moving(self):
         
         keys = pygame.key.get_pressed()
-# переработать систему стен (сделать их объектами)
         if keys[pygame.K_w] and keys[pygame.K_a]:
-
-            if not(self.x > 244 and self.x < 972):
-                if (self.y > 377 and self.y < 447): #ширина двери
-                    self.y -= self.diagonal_speed
-            elif self.y > 223 or (self.x > 570 and self.x < 650):
-                self.y -= self.diagonal_speed
-
-            if not(self.y > 222 and self.y < 614):
-                if (self.x > 570 and self.x < 646): #ширина двери
-                    self.x -= self.diagonal_speed
-            elif self.x > 250 or (self.y > 377 and self.y < 447):
+            if (self.x > 246 or (self.y > 378 and self.y + self.hitbox.height < 510)) and (self.y > 222 or (self.x > 574 and self.x + self.hitbox.width < 708)):
                 self.x -= self.diagonal_speed
-
+                self.y -= self.diagonal_speed
         elif keys[pygame.K_a] and keys[pygame.K_s]:
-
-            if not(self.y > 222 and self.y < 614):
-                if (self.x > 570 and self.x < 646): #ширина двери
-                    self.x -= self.diagonal_speed
-            elif self.x > 250 or (self.y > 377 and self.y < 447):
+            if (self.x > 246 or (self.y > 378 and self.y + self.hitbox.height < 510)) and (self.y < 614 or (self.x > 574 and self.x + self.hitbox.width < 708)):
                 self.x -= self.diagonal_speed
-
-            if not(self.x > 244 and self.x < 972):
-                if (self.y > 377 and self.y < 447): #ширина двери
-                    self.y += self.diagonal_speed
-            elif self.y < 610 or (self.x > 570 and self.x < 650):
                 self.y += self.diagonal_speed
-
         elif keys[pygame.K_s] and keys[pygame.K_d]:
-            if not(self.x > 244 and self.x < 972):
-                if (self.y > 377 and self.y < 447): #ширина двери
-                    self.y += self.diagonal_speed
-            elif self.y < 610 or (self.x > 570 and self.x < 650):
+            if (self.y < 614 or (self.x > 574 and self.x + self.hitbox.width < 708)) and (self.x < 970 or (self.y > 378 and self.y + self.hitbox.height < 510)):
+                self.x += self.diagonal_speed
                 self.y += self.diagonal_speed
-
-            if not(self.y > 222 and self.y < 614):
-                if (self.x > 565 and self.x < 645): #ширина двери
-                    self.x += self.diagonal_speed
-            elif self.x < 970 or (self.y > 377 and self.y < 447):
-                self.x += self.diagonal_speed
-                
         elif keys[pygame.K_d] and keys[pygame.K_w]:
-
-            if not(self.y > 222 and self.y < 614):
-                if (self.x > 565 and self.x < 645): #ширина двери
-                    self.x += self.diagonal_speed
-            elif self.x < 970 or (self.y > 377 and self.y < 447):
+            if (self.x < 970 or (self.y > 378 and self.y + self.hitbox.height < 510)) and (self.y > 222 or (self.x > 574 and self.x + self.hitbox.width < 708)):
                 self.x += self.diagonal_speed
-
-            if not(self.x > 244 and self.x < 972):
-                if (self.y > 377 and self.y < 447): #ширина двери
-                    self.y -= self.diagonal_speed
-            elif self.y > 223 or (self.x > 570 and self.x < 650):
                 self.y -= self.diagonal_speed
 
-        elif keys[pygame.K_a]:
-
-            if not(self.y > 222 and self.y < 614):
-                if (self.x > 570 and self.x < 646): #ширина двери
-                    self.x -= self.speed
-            elif self.x > 250 or (self.y > 377 and self.y < 447):
+        if keys[pygame.K_a]:
+            if self.x > 246 or (self.y > 378 and self.y + self.hitbox.height < 510):
                 self.x -= self.speed
-
         elif keys[pygame.K_d]:
-
-            if not(self.y > 222 and self.y < 614):
-                if (self.x > 565 and self.x < 645): #ширина двери
-                    self.x += self.speed
-            elif self.x < 970 or (self.y > 377 and self.y < 447):
+            if self.x < 970 or (self.y > 378 and self.y + self.hitbox.height < 510):
                 self.x += self.speed
-
         elif keys[pygame.K_s]:
-
-            if not(self.x > 244 and self.x < 972):
-                if (self.y > 377 and self.y < 447): #ширина двери
-                    self.y += self.speed
-            elif self.y < 610 or (self.x > 570 and self.x < 650):
+            if self.y < 614 or (self.x > 574 and self.x + self.hitbox.width < 708):
                 self.y += self.speed
-
         elif keys[pygame.K_w]:
-            if not(self.x > 244 and self.x < 972):
-                if (self.y > 377 and self.y < 447): #ширина двери
-                    self.y -= self.speed
-            elif self.y > 223 or (self.x > 570 and self.x < 650):
+            if self.y > 222 or (self.x > 574 and self.x + self.hitbox.width < 708):
                 self.y -= self.speed
 
 class Enemy(Character):
     def __init__(self, name, hp, damage, texture, hitbox, speed, diagonal_speed, x, y):
         super().__init__(name, texture, hitbox, hp, damage, speed, diagonal_speed, x, y)
+
 
 class Warrior(Player):
     def __init__(self, name, hp, damage, texture, hitbox, speed, diagonal_speed, x, y):
